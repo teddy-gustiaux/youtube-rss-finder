e24f84f7ede8e9e25d4dbb31f91b971ecf793eae
Teddy Gustiaux <code@gustiaux.com>
2018-07-10 22:31:41 -0400

    Make sure page action is display only for valid URLs

--- src/background.js
+++ src/background.js
@@ -1,80 +1,135 @@
 /*
- * ================================================================================
+ * =================================================================================================
  * CONSTANTS
- * ================================================================================
+ * =================================================================================================
  */
 
 const CONTENT_SCRIPT = '/content-script.js';
 const FEED_BASE_URL = 'https://www.youtube.com/feeds/videos.xml?';
 const YOUTUBE_WATCH = 'https://www.youtube.com/watch?';
+let globalFeed = null;
 
 /*
- * ================================================================================
+ * =================================================================================================
  * UTILITIES
- * ================================================================================
+ * =================================================================================================
  */
 
-function onError(error) {
-    console.log(`Error: ${error}`);
+async function getActiveTab() {
+    const tabs = await browser.tabs.query({ active: true, currentWindow: true });
+    if (typeof tabs !== 'undefined' && tabs.length > 0) {
+        return tabs[0];
+    }
+    return null;
+}
+
+function isValidURL(urlString) {
+    const supportedProtocols = ['https:', 'http:'];
+    const url = document.createElement('a');
+    url.href = urlString;
+    return supportedProtocols.indexOf(url.protocol) !== -1;
+}
+
+function isWatchURL(url) {
+    return url.substring(0, 30) === YOUTUBE_WATCH;
 }
 
 function getIdentifier(url, splitter) {
     return url.split(splitter)[1].split('/')[0];
 }
 
-function getFeed(url) {
+function buildFeedIdentifier(url, splitter, queryStringParameter) {
     let channel;
-    let id;
+    if (url.split(splitter)[1]) {
+        const id = getIdentifier(url, splitter);
+        if (id && typeof id !== 'undefined') channel = `${queryStringParameter}=${id}`;
+    }
+    return channel || null;
+}
+
+function buildFeed(url) {
+    let channel = null;
     if (url.split('channel/')[1]) {
-        id = getIdentifier(url, 'channel/')
-        if (id) channel = `channel_id=${id}`;
-    } else if(url.split('user/')[1]) {
-        id = getIdentifier(url, 'user/')
-        if (id) channel = `user=${id}`;
-    } else {
-        id = getIdentifier(url, 'youtube.com/')
-        if (id) channel = `user=${id}`;
+        channel = buildFeedIdentifier(url, 'channel/', 'channel_id');
+    } else if (url.split('user/')[1]) {
+        channel = buildFeedIdentifier(url, 'user/', 'user');
+    } else if (url.split('/').length === 4) {
+        channel = buildFeedIdentifier(url, 'youtube.com/', 'user');
     }
-    if (channel) {
+    if (channel !== null) {
         return FEED_BASE_URL + channel;
+    }
+    return null;
+}
+
+async function getFeed(currentURL) {
+    return buildFeed(currentURL);
+}
+
+async function getFeedForWatch() {
+    const result = await browser.tabs.executeScript({
+        file: CONTENT_SCRIPT,
+    });
+    return buildFeed(result[0]);
+}
+
+async function displayFeed(feed) {
+    if (isValidURL(feed)) {
+        browser.tabs.create({
+            url: feed,
+            active: true,
+        });
+    }
+}
+
+async function onPageActionClick(tab) {
+    if (isWatchURL(tab.url)) {
+        globalFeed = await getFeedForWatch();
+    }
+    displayFeed(globalFeed);
+}
+
+async function processUpdate() {
+    const activeTab = await getActiveTab();
+    if (activeTab === null) {
+        browser.pageAction.hide(activeTab.id);
+    } else if (isWatchURL(activeTab.url)) {
+        browser.pageAction.show(activeTab.id);
     } else {
-        return null;
-    }
-}
-
-async function displayFeed(tabs) {
-    if (tabs[0]) {
-        const currentURL = tabs[0].url;
-        let feed;
-        if (currentURL.substring(0, 30) === YOUTUBE_WATCH) {
-            const executing = browser.tabs.executeScript({
-                file: CONTENT_SCRIPT,
-            });
-            await executing.then(result => {
-                feed = getFeed(result[0]);
-            }, onError);
+        globalFeed = await getFeed(activeTab.url);
+        if (globalFeed !== null) {
+            browser.pageAction.show(activeTab.id);
         } else {
-            feed = getFeed(currentURL);
-        }
-        if (feed !== null) {
-            browser.tabs.create({
-                url: feed,
-                active: true,
-            });
+            browser.pageAction.hide(activeTab.id);
         }
     }
-}
-
-function buttonClicked() {
-    const gettingActiveTab = browser.tabs.query({ active: true, currentWindow: true });
-    gettingActiveTab.then(displayFeed, onError);
 }
 
 /*
- * ================================================================================
+ * =================================================================================================
  * LISTENERS
- * ================================================================================
+ * =================================================================================================
  */
 
+// -------------------------------------------------------------------------------------------------
+// TABS
+// -------------------------------------------------------------------------------------------------
+// Listen to tab URL changes
+browser.tabs.onUpdated.addListener(processUpdate, {
+    urls: ['https://youtube.com/*'],
+    properties: ['status'],
+});
+// Listen to tab activation and tab switching
+browser.tabs.onActivated.addListener(processUpdate);
+
+// -------------------------------------------------------------------------------------------------
+// WINDOWS
+// -------------------------------------------------------------------------------------------------
+// Listen for window activation and window switching
+browser.windows.onFocusChanged.addListener(processUpdate);
+
+// -------------------------------------------------------------------------------------------------
+// PAGE ACTION
+// -------------------------------------------------------------------------------------------------
 // Listen for clicks on the button
-browser.pageAction.onClicked.addListener(buttonClicked);
+browser.pageAction.onClicked.addListener(onPageActionClick);
